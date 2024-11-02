44ead76470edee7208f20adfdfe472c870a53dfa
Quang <leminhquangdev@gmail.com>
2024-11-02 21:08:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./page/HomePage/HomePage";
+import LoginPage from "./page/LoginPage/LoginPage";
+import Header from "./Component/Header/Header";
 import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
