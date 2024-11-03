480c1b383d8f2c221ac1bd2bec01f4e3ebf7d3a9
Quang <leminhquangdev@gmail.com>
2024-11-03 20:18:02 +0700

    app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./page/HomePage/HomePage";
-import LoginPage from "./page/LoginPage/LoginPage";
-import Header from "./Component/Header/Header";
-import "./App.css";
+
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 }
