348071b27853ca3d49b75887b74970a837b776a3
congvo-dev <conghtc97@gmail.com>
2024-11-04 11:38:42 +0700

    add spinner in interceptor

---
+++ src/component/Spinner/Spinner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { HashLoader } from "react-spinners";
+
+export default function Spinner() {
+  let { isLoading } = useSelector((state) => state.spinnerSlice);
+  console.log(isLoading);
+  return isLoading ? (
+    <div className="fixed h-screen w-screen top-0 left-0 bg-white z-20 flex justify-center items-center">
+      <HashLoader color="#48dd27" size={50} speedMultiplier={1} />
+    </div>
+  ) : (
+    <></>
+  );
+}
