ca8d8248f515ebf16d348f4477e8e47a85e6cd00
congvo-dev <conghtc97@gmail.com>
2024-11-05 09:37:31 +0700

    fix path Layout

--- src/template/Layout.jsx
+++ src/template/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../Component/Header/Header";
+import Header from "../component/Header/Header";
 export default function Layout({ content }) {
   return (
     <div>
