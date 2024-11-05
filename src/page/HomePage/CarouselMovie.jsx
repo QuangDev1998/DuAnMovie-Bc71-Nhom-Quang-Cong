a38397f8ee7b224cc8fe64a203dbae47d3e8d710
Quang <leminhquangdev@gmail.com>
2024-11-06 06:07:19 +0700

    update carousel

--- src/page/HomePage/CarouselMovie.jsx
+++ src/page/HomePage/CarouselMovie.jsx
@@ -4,12 +4,12 @@ import { useEffect, useState } from "react";
 import { movieService } from "../../service/userService";
 import { useNavigate } from "react-router-dom";
 const contentStyle = {
-  height: "400px",
-  color: "#DC2626",
-  lineHeight: "160px",
-  textAlign: "center",
+  // height: "400px",
+  // color: "#DC2626",
+  // lineHeight: "160px",
+  // textAlign: "center",
   backgroundPosition: "center",
-  backgroudSize: "100%",
+  backgroudSize: "cover",
   backgroudRepeat: "no-repeat",
 };
 export default function CarouselMovie() {
@@ -27,23 +27,14 @@ export default function CarouselMovie() {
     return movieArr.map((phim) => {
       return (
         <div
-          className=""
-          style={{ ...contentStyle, backgroundImage: `url(${phim.hinhAnh})` }}
+          className="h-screen"
+          // style={{ ...contentStyle, backgroundImage: `url(${phim.hinhAnh})` }}
         >
-          <img
-            className="w-full h-85 object-cover rounded-lg"
-            src={phim.hinhAnh}
-            alt=""
-          />
+          <img className="w-full h-full" src={phim.hinhAnh} alt="" />
         </div>
       );
     });
   };
 
-  return (
-    <div>
-      {" "}
-      <Carousel autoplay>{renderMovie()}</Carousel>
-    </div>
-  );
+  return <Carousel draggable>{renderMovie()}</Carousel>;
 }
