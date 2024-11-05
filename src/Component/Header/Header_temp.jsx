aa9ea6a71910064c8225c9fe943a44ab3d280abb
congvo-dev <conghtc97@gmail.com>
2024-11-05 09:44:45 +0700

    Rename to correct capitalization

--- src/Component/Header/Header_temp.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <div className="">
-      <div className="container h-20 flex justify-between"></div>
-    </div>
-  );
-}
