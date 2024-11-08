02965f22f7a75fba18d1208bb491a88930ea85ca
Quang <leminhquangdev@gmail.com>
2024-11-08 18:02:51 +0700

    update login

--- src/page/User/LoginPage/LoginPage.jsx
+++ src/page/User/LoginPage/LoginPage.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import FormLogin from "./FormLogin";
 import bgImg from "../../../asset/movie-poster-bg.jpg";
+
 export default function LoginPage() {
+  const user = useSelector((state) => state.userSlice.loginData);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Nếu người dùng đã đăng nhập, điều hướng về trang chủ
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
   return (
     <div className="relative">
       {/* background image */}
@@ -15,11 +28,9 @@ export default function LoginPage() {
         }}
         className="h-screen w-screen z-0 fixed top-0 left-0"
       ></div>
-      <div className="z-10 fixed top-0 left-0 h-screen w-screen flex justify-center items-center ">
+      <div className="z-10 fixed top-0 left-0 h-screen w-screen flex justify-center items-center">
         <FormLogin />
       </div>
     </div>
   );
 }
-
-// flex justify-center items-center h-screen
